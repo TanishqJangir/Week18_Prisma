c2312656a4d47fc08567f67f64d8ba8cdd1eb362
TanishqJangir <tanishqjangir001@gmail.com>
2026-01-21 22:36:15 +0530

    seed init

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,34 +5,17 @@ const client = new PrismaClient();
 async function createDummyUser() {
     await client.users.create({
         data: {
-            username: "TanishqJangir",
+            username: "John",
             password: "123123123",
             todos: {
                 create: {
-                    title: "Go to college",
-                    description: "at sharp 10 am",
+                    title: "Go to office",
+                    description: "123123",
                     done: false,
                 }
             }
         }
     })
-
-    // const response = await client.users.findFirst({
-    //     where : {
-    //         username : "TanishqJangir"
-    //     }
-    // })
-
-    // const id = Number(response?.id);
-
-    // await client.todos.create({
-    //     data : {
-    //         title : "Go to college",
-    //         description : "at sharp 10 am",
-    //         done : false,
-    //         userId : id
-    //     }
-    // })
 
     console.log("Dummy data inserted")
 }
